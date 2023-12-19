24b3a383b133d9ab5ab593ecb95357439b66f654
ssKazal <salmansiddik19@outlook.com>
2023-12-20 00:13:28 +0600

    Products table data in list page

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,23 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Product, Variant
 
 
 class CreateProductView(generic.TemplateView):
-    template_name = 'products/create.html'
+    template_name = "products/create.html"
 
     def get_context_data(self, **kwargs):
         context = super(CreateProductView, self).get_context_data(**kwargs)
-        variants = Variant.objects.filter(active=True).values('id', 'title')
-        context['product'] = True
-        context['variants'] = list(variants.all())
+        variants = Variant.objects.filter(active=True).values("id", "title")
+        context["product"] = True
+        context["variants"] = list(variants.all())
+        return context
+
+
+class ProductListView(generic.TemplateView):
+    template_name = "products/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context["products"] = Product.objects.all()
         return context
