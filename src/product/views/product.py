1f20c7315d638afe8dedb09dbaf75eb5310f42a8
ssKazal <salmansiddik19@outlook.com>
2023-12-20 10:31:56 +0600

    Filter part

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from django.db.models import Q
 
-from product.models import Product, Variant
+from product.models import Product, ProductVariant ,Variant
 
 
 class CreateProductView(generic.TemplateView):
@@ -21,4 +22,46 @@ class ProductListView(generic.ListView):
     paginate_by = 5  # Set the number of items per page
 
     def get_queryset(self):
-        return Product.objects.all()
+        queryset = Product.objects.all()
+
+        title = self.request.GET.get('title')
+        variant = self.request.GET.get('variant')
+        price_from = self.request.GET.get('price_from')
+        price_to = self.request.GET.get('price_to')
+        date = self.request.GET.get('date')
+
+        # Apply filters to queryset based on form inputs
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if variant:
+            queryset = queryset.filter(productvariant__variant_title=variant).distinct()
+            print(queryset)
+
+        if price_from:
+            queryset = queryset.filter(
+                Q(productvariantprice__price__gte=float(price_from))
+            ).distinct()
+
+        if price_to:
+            queryset = queryset.filter(
+                Q(productvariantprice__price__lte=float(price_to))
+            ).distinct()
+
+        if date:
+            queryset = queryset.filter(created_at__date=date).distinct()
+
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+
+        # Get distinct product variant titles for each variant
+        variant_titles_by_variant = {}
+        for variant in Variant.objects.all():
+            variant_titles = ProductVariant.objects.filter(variant=variant).values_list('variant_title', flat=True).distinct()
+            variant_titles_by_variant[variant.title] = variant_titles
+
+        context['variant_titles_by_variant'] = variant_titles_by_variant
+
+        return context
